21b052bb64258e06fcd03dffb8d93b74ffb5c35d
Luca Reverberi (lukeku62) <prevebbo@gmail.com>
2022-02-24 17:35:16 +0100

    1.1.0

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 # JSON CEREALIZER
+
 ### A simple npm package to assign an id to every object of a json array
 
 ## Installation:
+
 npm i jsoncereal
 
 ## Usage example:
+
 const array = [{'json array'}]
 
 const serializedArray = makeId(array)
 
+## Updates:
+
+### 1.1.0
+
+--> new function makeIdCustom(), that is equal to makeId() but the id property name is customizable
+<br/>
+--> updated exports method
+
 ### 1.0.1
-fixed module.exports
+
+--> fixed module.exports
--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const { v4 } = require("uuid");
-makeId = (data) => {
-    data.map((d) => (d["_id"] = v4()));
-    return data;
+export const { v4 } = require("uuid");
+
+/**
+ * INPUT: a javascript array of objects
+ * OUTPUT: the same array, but every object has a new property
+ * with key: _id and value: a randomly generated string
+ */
+makeId = (array) => {
+    array.map((d) => (d["_id"] = v4()));
+    return array;
 };
 
-module.exports = makeId;
+/**
+ * INPUT: a javascript array of objects and a preferred name for key id
+ * OUTPUT: the same array, but every object has a new property
+ * with key: _id and value: a randomly generated string
+ */
+export const makeIdCustom = (array, key) => {
+    array.map((a) => (a[key] = v4()));
+    return array;
+};
